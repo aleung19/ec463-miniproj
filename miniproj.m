dcad5b038a7733e5e20f6d881bdca5db1eab45c1
aleung19 <43117758+aleung19@users.noreply.github.com>
2018-09-15 17:05:45 -0400

    Update miniproj.m

--- miniproj.m
+++ miniproj.m
@@ -3,64 +3,70 @@ v = vision.VideoFileReader('carfootage-short.mp4'); % read footage
 detector = vision.ForegroundDetector('NumGaussians', 5, ...
     'NumTrainingFrames', 70); % creates foreground mask
 blob = vision.BlobAnalysis('AreaOutputPort', false, ...
-     'BoundingBoxOutputPort', true, 'CentroidOutputPort', true, ...
-     'MinimumBlobArea', 3500); % analyzes connected regions in the mask
-olddetects = []; 
-carcandidates = struct('id',{},'bboxes',{},'framespresent',{},'framesgone',{});
-% initialize matrix of tracks
-numcars = 0; % initialize counter
+     'BoundingBoxOutputPort', true, 'CentroidOutputPort', false, ...
+     'MinimumBlobArea', 3500); % analyzes connected regions in the mask 
+carcandidates = struct('bboxes',{},'framespresent',{},'framesgone',{});
+% each entry in this structure holds detections that are candidates to be
+% legitimate car tracking
 se = strel('square', 3);
 videoPlayer = vision.VideoPlayer();
 while ~isDone(v)
     frame = v();
     foreground = detector(frame);
-    foreground1 = bwareaopen(foreground, 500);
-    foregroundfilt = imopen(foreground1, se);
-    [CENTROID, BBOX] = step(blob, foregroundfilt);
-    newdetects = BBOX;
-    if isempty(olddetects) == 1
-        olddetects = BBOX;
-    end
+    foreground1 = bwareaopen(foreground, 500); % noise filter based on size
+    foregroundfilt = imopen(foreground1, se); % algorithm noise filter
+    BBOX = step(blob, foregroundfilt); % Bounding box coords
+    newdetects = BBOX; % We'll consider these bounding boxes new detections
     if isempty(carcandidates) == 1
         for m = 1:size(BBOX,1)
             carcandidates(m).bboxes = BBOX(m,:);
+            carcandidates(m).framespresent = 1;
+            carcandidates(m).framesgone = 0;
         end
-    end
-    overlapRatio = bboxOverlapRatio(olddetects, newdetects);
-    % if any ratio > .5, take column # which indicates nowtracks
-    detectcarcand = overlapRatio > 0.5;
-    carcandindex = any(detectcarcand);
-%     if sum(trackindex) > 0
-    olddetects = [];
-        for i = 1:sum(carcandindex)
-            if carcandindex(i) == 1
-                olddetects = [BBOX(i,:)];
-                nolddetects = size(olddetects,1);
-                ncarcandidates = numel(carcandidates);
-                for j = 1:nolddetects
-                    for k = 1:ncarcandidates
-                    if bboxOverlapRatio(olddetects(j,:),carcandidates(k).bboxes(size(carcandidates(k).bboxes,1),:)) > 0.5
-                        carcandidates(k).bboxes = [carcandidates(k).bboxes; olddetects(j,:)];
-                    end
-                    end
+    end % On the first frame of detections, we'll consider all of them
+    % candidates -- need to adjust in case first frame has no detections
+    activetracks = carcandidates; % Initialize all car candidates as active
+    % tracks (tracks that are onscreen)
+    nactivetracks = numel(activetracks);
+    nnewdetects = size(newdetects,1); 
+    for j = 1:nnewddetects
+        for k = 1:ncarcandidates
+            activetracks = carcandidates;
+            if activetracks(k).framesgone > 15
+                activetracks(k).framesgone = []; % if a track is gone for
+                % over a certain amount of frames, it's removed from the
+                % list of active tracks
+            end
+            if bboxOverlapRatio(newdetects(j,:),carcandidates(k).bboxes(size(carcandidates(k).bboxes,1),:)) > 0.5
+                carcandidates(k).bboxes = [carcandidates(k).bboxes; newdetects(j,:)];
+                carcandidates(k).framespresent = carcandidates(k).framespresent + 1;
+                % new tracks that are candidates for cars are established
+                % if bounding boxes over consecutive frames overlap each
+                % other over a certain percentage
+            else
+                if carcandidates(k).framespresent > 0
+                    carcandidates(k).framesgone = carcandidates(k).framesgone + 1;
                 end
+                carcandidates(end + 1).bboxes = newdetects(j,:); % problematic looping prob here ...
+                % looping through k and adding newdetects(j,:) to
+                % carcandidates over and over again?
+                carcandidates(end).framespresent = 1;
+                carcandidates(end).framesgone = 0;
             end
         end
-%     end
-    % put all detects into car candidates, if frame appearance greater than
-    % 60, is a car, +1 num cars. delete candidates that appear less than 60
-%     numcars = numcars + sum(lostdetects);
-    shapeInserter = vision.ShapeInserter('BorderColor', 'White');
-    out = shapeInserter(frame, BBOX);
-    videoPlayer(out);
-    % step function somewhere?
-end
-    for l = 1:numel(carcandidates)
-        if size(carcandidates(l).bboxes,1) < 10
-            carcanddiates(l) = [];
-        end
     end
-    numcars = numel(carcandidates);
+    shapeInserter = vision.ShapeInserter('BorderColor', 'White');
+    out = shapeInserter(frame, BBOX); % adds bounding boxes to video
+    videoPlayer(out);
+end
+    for n = 1:numel(carcandidates)
+        if size(carcandidates(n).bboxes,1) < 10
+            carcanddiates(n) = [];
+        end
+    end % eliminating car candidates whose tracks did not last over a
+    % certain # of frames
+    numcars = numel(carcandidates); % hopefully, # of car candidates is
+    % actual # of cars
 % Will need to adjust inputs from Raspberry Pi Camera
 % Need to perfect a cleaning method, false detections high, cleaning noise
 % dependent on size which is not versatile
