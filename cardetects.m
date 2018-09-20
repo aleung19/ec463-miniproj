76e5cef72ad1372ad0c2dba80574e9d51e7ddc39
aleung19 <43117758+aleung19@users.noreply.github.com>
2018-09-20 10:38:23 -0400

    Update and rename miniproj.m to cardetects.m

---
+++ cardetects.m
@@ -0,0 +1,27 @@
+v = vision.VideoFileReader(videofile); % read footage
+detector = vision.ForegroundDetector('NumGaussians', 5, ...
+    'NumTrainingFrames', 70); % creates foreground mask
+blob = vision.BlobAnalysis('AreaOutputPort', false, ...
+     'BoundingBoxOutputPort', true, 'CentroidOutputPort', false, ...
+     'MinimumBlobArea', 3500); % analyzes connected regions in the mask 
+carcandidates = struct('id',{},'bboxes',{},'framesgone',{},'timestamp', ... 
+    {}, 'cargone',{});
+activetracks = carcandidates;
+carsfinal = {};
+% each entry in this structure holds detections that are candidates to be
+% legitimate car tracking
+se = strel('square', 3);
+numcars = 0;
+videoPlayer = vision.VideoPlayer();
+newtracks = [];
+framecounter = 0;
+[carcandidates, activetracks] = processvid(v, detector, blob, ...
+    carcandidates, activetracks, se, numcars, videoPlayer, newtracks, ...
+    framecounter, framerate);
+carsfinal = finalizecars(carsfinal, numcars, activetracks, carcandidates);
+detectedcars = cell2mat(carsfinal);
+csvwrite('carsandtimestamps.txt', detectedcars);
+type('carsandtimestamps.txt')
+% We'll only consider a track a legitimate car candidate if it
+% appears over a certain number of frames
+end
